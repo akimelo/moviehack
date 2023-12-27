1aa09f0f1b6e110b818b7ce46d680912fac77fe1
akiJacky <tyatya38an@gmail.com>
2023-12-28 00:48:50 +0900

    Initial Commit

---
+++ moviehack_ios/moviehack_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  moviehack_iosApp.swift
+//  moviehack_ios
+//
+//  Created by Akihiro Nakano on 2023/12/28.
+//
+
+import SwiftUI
+
+@main
+struct moviehack_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
